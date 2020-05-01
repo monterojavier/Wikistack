cceb48f34b90a74f2350f854d310ee89749e93d0
Javier Montero <monterodotjavier@icloud.com>
2020-04-30 21:55:00 -0400

    init

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const { db, Page, User } = require("./models");
 
 const app = express();
 
@@ -8,4 +9,26 @@ app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+const init = async () => {
+  try {
+    await Page.sync();
+    await User.sync();
+    // await db.sync({force: true})
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+init();
+
+const PORT = 3000;
+
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
+
+app.listen(PORT, () => {
+  console.log("It's bitching");
 });
